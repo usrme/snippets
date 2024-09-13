4a8c06e16ec1c93416f5dec6a4bb0c5efa412f4f
Üllar Seerme <yates@protonmail.ch>
2024-09-13 16:35:50 +0300

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,36 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Back to home page',
+      customCss: [
+        './src/styles/starlight.css',
+      ],
+      sidebar: [
+        {
+          label: 'Start here',
+          items: [
+            { label: 'Ansible', link: '/snippets/ansible' },
+            { label: 'Docker/Podman', link: '/snippets/docker-podman' },
+            { label: 'Git', link: '/snippets/git' },
+            { label: 'Kubernetes', link: '/snippets/kubernetes' },
+            { label: 'PowerShell', link: '/snippets/powershell' },
+            { label: 'Shell', link: '/snippets/shell' },
+            { label: 'Vim', link: '/snippets/vim' },
+          ],
+        },
+      ],
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            src: '//gc.zgo.at/count.js',
+            'data-goatcounter': 'https://usrme.goatcounter.com/count',
+            defer: true,
+          },
+        },
+      ],
+    }),
+  ],
 });
